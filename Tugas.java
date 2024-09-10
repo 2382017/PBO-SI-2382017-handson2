b72579fef50822025e0253cab90b4151a6747d8e
Rawely Naibaho <2382017@unai.edu>
2024-09-10 10:36:55 +0700

    Handson2

---
+++ Tugas.java
@@ -0,0 +1,7 @@
+public class Tugas {
+    public static void main(String[] args) {
+        //buatlah sebuah progaram yang menagmbil 2 input number dasn user
+        //program tersebut akan print out nilai yang paling besar
+
+    }
+}
